3d4398a887d532522dc37b052f8f86c0865a55af
LongNTDev <codeinlife10@gmail.com>
2023-03-30 09:07:12 +0700

    ok

--- index.js
+++ index.js
@@ -48,50 +48,6 @@
 // // Start the camera
 // camera.start();
 
-// function onResults(results) {
-//   canvasCtx.save();
-//   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-//   canvasCtx.drawImage(
-//     results.image,
-//     0,
-//     0,
-//     canvasElement.width,
-//     canvasElement.height
-//   );
-//   if (results.multiHandLandmarks) {
-//     for (const landmarks of results.multiHandLandmarks) {
-//       drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
-//         color: "#00FF00",
-//         lineWidth: 5,
-//       });
-//       drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
-//     }
-//   }
-//   canvasCtx.restore();
-// }
-
-// const hands = new Hands({
-//   locateFile: (file) => {
-//     return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-//   },
-// });
-// hands.setOptions({
-//   maxNumHands: 2,
-//   modelComplexity: 1,
-//   minDetectionConfidence: 0.5,
-//   minTrackingConfidence: 0.5,
-// });
-// hands.onResults(onResults);
-
-// const camera = new Camera(videoElement, {
-//   onFrame: async () => {
-//     await hands.send({ image: videoElement });
-//   },
-//   width: 1280,
-//   height: 720,
-// });
-// camera.start();
-
 
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
@@ -134,6 +90,7 @@ function onResults(results) {
 const holistic = new Holistic({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
 }});
+
 holistic.setOptions({
   modelComplexity: 1,
   smoothLandmarks: true,
